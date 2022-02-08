187edf8c4d9f90bff443382e2b6e1cc6a99a9334
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2022-02-08 23:46:11 +0530

    feat: complete feature to send requests to server and printing the HTTP request

---
+++ README.md
@@ -0,0 +1,8 @@
+# Single Threaded Web Server in Rust
+
+A single threaded web server written in Rust.
+
+Features:
+
+- Sending a request from the browser(client) to the server.
+- Receiving a response from the server after a request has been sent.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[warn(unused_variables)]
+
+use std::io::prelude::*;
+// importing TCP listener
+use std::net::{TcpListener, TcpStream};
+
+
 fn main() {
-    println!("Hello, world!");
+    // creating a TCP listener
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream: TcpStream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
