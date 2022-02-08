82992677d3d0b89c1fa6de50f95f885dfa12d6ed
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2022-02-08 23:59:46 +0530

    feat: add feature to send response back to the client!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 use std::io::prelude::*;
 // importing TCP listener
 use std::net::{TcpListener, TcpStream};
+// fs module
+use std::fs;
 
 
 fn main() {
@@ -17,9 +19,22 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    // the buffer storing the request data
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // reading contents from the `index.html` file using the fs module
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    // hard coding the first line of the response
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    // write the response back to the client
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
